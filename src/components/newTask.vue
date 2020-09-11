e474598c3fb6340eebb8eb4709f8eebab3bf6a1d
Leonardo Huttner <leonardohuttner@gmail.com>
2020-09-11 16:33:43 -0300

    Pronto

---
+++ src/components/newTask.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="new-class">
+      <input v-model="name" @keydown.enter="add" type="text"
+      class='form-element' placeholder="Do It">
+      <button @click="add" class='form-element'>+</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            name:''
+        }
+    },
+    methods:{
+        add(){
+            this.$emit('taskAdded',{name: this.name})
+            this.name = ''
+        }
+    }
+}
+</script>
+
+<style>
+.new-task{
+    margin:35px;
+}
+.form-element{
+    outline:none;
+    font-size: 2rem;
+    border: 1px solid #ffffff;
+    padding: 5px 10px 8px;
+    color:#ffffff
+}
+input.form-element{
+    width: 500px;
+    background-color: rgba(255, 255, 255, 0.055);
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+}
+button.form-element{
+    border-left:none;
+    background-color: #2196f3;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+}
+</style>
---
+++ src/components/taskGrid.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="task-grid">
+    <template v-if="tasks.length">
+        <Task v-for="(task,i) in tasks" :task="task" 
+        @taskDeleted="$emit('taskDeleted',i)"
+        @taskStateChanged="$emit('taskStateChanged',i)"
+        :key='task.name'></Task>
+    </template>
+    <p v-else class="no-task">Enjoy</p>
+</div>
+</template>
+    
+<script>
+import Task from './Task.vue'
+
+export default {
+    components:{Task},
+    props:{
+        tasks:{type:Array,required:true}
+    }
+}
+</script>
+
+<style>
+.task-grid{
+    display:flex;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+.task-grid .task{
+    margin: 10px;
+
+}
+.no-task{
+    color: #aaaaaa;
+    font-size: 1.7rem;
+}
+</style>
